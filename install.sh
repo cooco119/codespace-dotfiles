c01d9fdae43a97ca50c255f05160f5f2a977eaa2
JaeJun.Lee <34636736+cooco119@users.noreply.github.com>
2023-07-14 15:37:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # should use powerline fonts
+# For iterm2:  https://apple.stackexchange.com/questions/368603/how-to-make-powerline-fonts-work-with-iterm2
 
 cp -f .zshrc ~/.zshrc
